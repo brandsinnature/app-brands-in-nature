8124d676a27c4423f957d313e0a811e0597be2c7
Pankaj Yadav <pankajyadav.code@gmail.com>
2024-09-02 11:17:45 +0530

    auth check

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -1,12 +1,5 @@
-"use client";
 import Container from "@/components/ui/container";
-import { BarcodeScanner } from "react-barcode-scanner";
-import "react-barcode-scanner/polyfill";
 
 export default function Home() {
-    return (
-        <Container>
-            <BarcodeScanner />
-        </Container>
-    );
+    return <Container>Home</Container>;
 }
--- app/(main)/scan/scan-rcc.tsx
+++ app/(main)/scan/scan-rcc.tsx
@@ -2,91 +2,100 @@
 
 import { Button } from "@/components/ui/button";
 import { CameraIcon, SwitchCamera, Zap, ZapOff } from "lucide-react";
-import { useRef, useState } from "react";
-import { Camera, CameraType } from "react-camera-pro";
+import { useEffect, useState } from "react";
+import { useZxing } from "react-zxing";
+import { toast } from "sonner";
 
 export default function ScanRcc() {
-    const camera = useRef<CameraType>(null);
+    const [result, setResult] = useState("");
+    const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+    const {
+        ref,
+        torch: { on, off, isOn, isAvailable },
+    } = useZxing({
+        onDecodeResult(result) {
+            setResult(result.getText());
+        },
+    });
 
-    const [numberOfCameras, setNumberOfCameras] = useState(0);
-    const [torchToggled, setTorchToggled] = useState<boolean>(false);
-    const [image, setImage] = useState<string | null>(null);
+    // useEffect(() => {
+    //     (async () => {
+    //         try {
+    //             const availableDevices =
+    //                 await navigator.mediaDevices.enumerateDevices();
+    //             const availableVideoDevices = availableDevices.filter(
+    //                 (device) => device.kind === "videoinput"
+    //             );
+    //             if (availableVideoDevices.length === 0)
+    //                 return toast.error("No cameras found");
+    //             setDevices(availableVideoDevices);
+    //         } catch (e) {
+    //             toast.error(
+    //                 "Failed to find cameras. This could be permissions problem"
+    //             );
+    //         }
+    //     })();
+    // }, []);
 
     return (
         <div>
-            <Camera
-                ref={camera}
-                aspectRatio="cover"
-                facingMode="environment"
-                numberOfCamerasCallback={(i) => setNumberOfCameras(i)}
-                errorMessages={{
-                    noCameraAccessible:
-                        "No camera device accessible. Please connect your camera or try a different browser.",
-                    permissionDenied:
-                        "Permission denied. Please refresh and give camera permission.",
-                    switchCamera:
-                        "It is not possible to switch camera to different one because there is only one video device accessible.",
-                    canvas: "Canvas is not supported.",
-                }}
-                videoReadyCallback={() => {
-                    console.log("Video feed ready.");
-                }}
-            />
+            <video ref={ref} />
 
-            <div className="">
-                <Button
-                    size={"icon-lg"}
-                    className="bg-white/80 rounded-full fixed left-1/2 bottom-16 transform -translate-x-1/2"
-                    onClick={() => {
-                        if (camera.current) {
-                            const photo = camera.current.takePhoto();
-                            setImage(photo as string);
-                        }
-                    }}
-                >
-                    <CameraIcon size={30} />
-                </Button>
-
-                <div className="right-5 bottom-16 z-50 fixed flex items-center gap-5">
-                    <Button
-                        size={"icon"}
-                        className="bg-black/80 dark:bg-white/80 rounded-full"
-                        disabled={numberOfCameras <= 1}
-                        onClick={() => {
-                            if (camera.current) camera.current.switchCamera();
-                        }}
-                    >
-                        <SwitchCamera />
-                    </Button>
-
-                    {camera.current?.torchSupported && (
-                        <Button
-                            size={"icon"}
-                            className="bg-black/80 dark:bg-white/80 rounded-full"
-                            onClick={() => {
-                                if (camera.current) {
-                                    setTorchToggled(
-                                        camera.current.toggleTorch()
-                                    );
-                                }
-                            }}
-                        >
-                            {torchToggled ? <Zap /> : <ZapOff />}
-                        </Button>
-                    )}
-                </div>
-            </div>
+            <p>
+                <span>Last result:</span>
+                <span>{result}</span>
+            </p>
+            {isAvailable ? (
+                <button onClick={() => (isOn ? off() : on())}>
+                    {isOn ? "Turn off" : "Turn on"} torch
+                </button>
+            ) : (
+                <strong>
+                    Unfortunately, torch is not available on this device.
+                </strong>
+            )}
         </div>
     );
 }
 
-{
-    /* <TorchButton
-    className={torchToggled ? "toggled" : ""}
-    onClick={() => {
-        if (camera.current) {
-            setTorchToggled(camera.current.toggleTorch());
-        }
-    }}
-/>; */
-}
+//  <div className="">
+//      <Button
+//          size={"icon-lg"}
+//          className="bg-white/80 rounded-full fixed left-1/2 bottom-16 transform -translate-x-1/2"
+//          onClick={() => {
+//              if (camera.current) {
+//                  const photo = camera.current.takePhoto();
+//                  setImage(photo as string);
+//              }
+//          }}
+//      >
+//          <CameraIcon size={30} />
+//      </Button>
+
+//      <div className="right-5 bottom-16 z-50 fixed flex items-center gap-5">
+//          <Button
+//              size={"icon"}
+//              className="bg-black/80 dark:bg-white/80 rounded-full"
+//              disabled={numberOfCameras <= 1}
+//              onClick={() => {
+//                  if (camera.current) camera.current.switchCamera();
+//              }}
+//          >
+//              <SwitchCamera />
+//          </Button>
+
+//          {camera.current?.torchSupported && (
+//              <Button
+//                  size={"icon"}
+//                  className="bg-black/80 dark:bg-white/80 rounded-full"
+//                  onClick={() => {
+//                      if (camera.current) {
+//                          setTorchToggled(camera.current.toggleTorch());
+//                      }
+//                  }}
+//              >
+//                  {torchToggled ? <Zap /> : <ZapOff />}
+//              </Button>
+//          )}
+//      </div>
+//  </div>;
