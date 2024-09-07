5be63b13968c4a36d9d302ab7f61545caa8047b9
Pankaj Yadav <pankajyadav.code@gmail.com>
2024-09-07 21:21:34 +0530

    build fix

--- app/(main)/scan/[code]/page.tsx
+++ app/(main)/scan/[code]/page.tsx
@@ -5,14 +5,6 @@ export const metadata: Metadata = {
     title: "Create a Product",
 };
 
-type Props = {
-    params: {
-        code: string;
-    };
-};
-
-export default function CreateProduct({ params }: Props) {
-    const { code } = params;
-
-    return <CreateProductForm code={code} />;
+export default function CreateProduct() {
+    return <CreateProductForm />;
 }
