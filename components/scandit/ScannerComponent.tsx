e44eebfb5a73e99e5da17b4da6fab4777bb53de4
brandsinnature <brandsinnature@gmail.com>
2025-02-24 12:27:33 +0530

    check

--- components/scandit/ScannerComponent.tsx
+++ components/scandit/ScannerComponent.tsx
@@ -80,6 +80,8 @@ export default function ScannerComponent() {
                 throw new Error("Failed to capture frame");
             }
 
+            console.log("Hello")
+
             const response = await fetch("https://scanner-service-oe4l.onrender.com/scan", {
                 method: 'POST',
                 headers: {
