0672952601137eb90f920916057cb4bba0507d62
Yogesh <brandsinnature@gmail.com>
2025-10-15 22:26:52 +0530

    Update

--- data-access/auth.ts
+++ data-access/auth.ts
@@ -7,69 +7,69 @@ import { jwtDecode } from "jwt-decode";
 import { redirect } from "next/navigation";
 
 export async function login(loginForm: ILoginForm) {
-    const client = createClient();
+  const client = createClient();
 
-    console.log(`Redirect link: ${process.env.META_URL}/scan`)
+  console.log(`Redirect link: ${process.env.META_URL}/scan`);
 
-    const { error } = await client.auth.signInWithOtp({
-        ...loginForm,
-        options: {
-            shouldCreateUser: true,
-            emailRedirectTo: `${process.env.META_URL}/scan`,
-        },
-    });
+  const { error } = await client.auth.signInWithOtp({
+    ...loginForm,
+    options: {
+      shouldCreateUser: true,
+      emailRedirectTo: `${process.env.META_URL}/auth/confirm?next=/scan`,
+    },
+  });
 
-    if (error) return { error: error.message };
+  if (error) return { error: error.message };
 
-    return { message: "Magic link sent to your mail" };
+  return { message: "Magic link sent to your mail" };
 }
 
 export async function getCurrentUser() {
-    const client = createClient();
+  const client = createClient();
 
-    const { data, error } = await client.auth.getUser();
+  const { data, error } = await client.auth.getUser();
 
-    if (error) return null;
+  if (error) return null;
 
-    return data.user;
+  return data.user;
 }
 
 export async function logout() {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const { error } = await supabase.auth.signOut();
+  const { error } = await supabase.auth.signOut();
 
-    if (error) return { error: error.message };
+  if (error) return { error: error.message };
 
-    redirect("/login");
+  redirect("/login");
 }
 
 export async function getSessionUserId() {
-    const client = createClient();
+  const client = createClient();
 
-    const {
-        data: { session },
-        error,
-    } = await client.auth.getSession();
+  const {
+    data: { session },
+    error,
+  } = await client.auth.getSession();
 
-    if (error) return null;
+  if (error) return null;
 
-    const jwt: IJwtPayload = jwtDecode(session?.access_token || "");
+  const jwt: IJwtPayload = jwtDecode(session?.access_token || "");
 
-    return jwt?.sub;
+  return jwt?.sub;
 }
 
 export async function getSessionUser() {
-    const client = createClient();
+  const client = createClient();
 
-    const {
-        data: { session },
-        error,
-    } = await client.auth.getSession();
+  const {
+    data: { session },
+    error,
+  } = await client.auth.getSession();
 
-    if (error) return null;
+  if (error) return null;
 
-    const user: IJwtPayload = jwtDecode(session?.access_token || "");
+  const user: IJwtPayload = jwtDecode(session?.access_token || "");
 
-    return user;
+  return user;
 }
